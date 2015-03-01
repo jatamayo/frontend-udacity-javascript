aafc94d979428b1453976c205e6fd3d4ee0bf462
alejandro tamayo <jalejandrotamayo@hotmail.com>
2015-03-01 10:16:16 -0600

    terminado

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# frontend-udacity-javascript
-Complete Udacity's course [Intro to Javascript basics](https://www.udacity.com/course/ud804)
+# frontend-udacity-html-css
+Complete Udacity's course HTML &amp; CSS final project.
 
-## Final project
-- For final project, fork this repository
-- Complete the project and push it to your Github account
-- Make Pull-Request to Cognits
-
-
+## Requirements
+- Complete all the videos, quizzes in the Udacity course [HTML &amp; CSS](https://www.udacity.com/course/ud304).
+- Build final project with fork of this repository
+- Make a pull request to cognits when you're don
---
+++ js/script.js
@@ -0,0 +1,52 @@
+/*******************************************************/
+var datos={};
+datos.name="Alejandro Tamayo";
+datos.job="Web Developer";
+datos.mobile="5554-4433";
+datos.email="jatamayo@gmail.com";
+datos.github="jatamayo";
+datos.twitter="jatamayo";
+datos.location="Mixco";
+datos.description="it is a little description about me etc etc etc....!!!";
+datos.skills1="Awesome";
+datos.skills2="Helpmate";
+datos.skills3="Responsible";
+datos.skills4="sleepyhead";
+/***************IMPRIMIR NOMBRE**************************/
+miName= document.getElementById('name');
+miName.innerHTML = datos.name;
+/***************IMPRIMIR PROFESION***********************/
+miJob= document.getElementById('job');
+miJob.innerHTML = datos.job;
+/***************IMPRIMIR TELEFONO************************/
+mimobile= document.getElementById('mobile');
+mimobile.innerHTML = datos.mobile;
+/***************IMPRIMIR EMAIL***************************/
+miemail= document.getElementById('email');
+miemail.innerHTML = datos.email;
+/***************IMPRIMIR GITHUB**************************/
+migithub= document.getElementById('github');
+migithub.innerHTML = datos.github;
+/***************IMPRIMIR TWITTER*************************/
+mitwitter= document.getElementById('twitter');
+mitwitter.innerHTML = datos.twitter;
+/***************IMPRIMIR LOCATION************************/
+milocation= document.getElementById('location');
+milocation.innerHTML = datos.location;
+/***************IMPRIMIR DESCRIPTION*********************/
+midescription= document.getElementById('description');
+midescription.innerHTML = datos.description;
+/***************IMPRIMIR HABILIDAD1**********************/
+miskills1= document.getElementById('skills1');
+miskills1.innerHTML = datos.skills1;
+/***************IMPRIMIR HABILIDAD2**********************/
+miskills2= document.getElementById('skills2');
+miskills2.innerHTML = datos.skills2;
+/***************IMPRIMIR HABILIDAD3**********************/
+miskills3= document.getElementById('skills3');
+miskills3.innerHTML = datos.skills3;
+/***************IMPRIMIR HABILIDAD4**********************/
+miskills4= document.getElementById('skills4');
+miskills4.innerHTML = datos.skills4;
+
+
